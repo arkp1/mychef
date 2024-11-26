0884388808edeb9ff8f5fa739622773452f874a2
praneet <praneet18.ark@gmail.com>
2024-11-27 02:40:21 +0530

    loading button update

--- src/Components/IngredientsList.jsx
+++ src/Components/IngredientsList.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 export default function IngredientsList(props) {
   const ingredientsListItems = props.ingredients.map((ingredient) => (
     <li key={ingredient}>{ingredient}</li>
   ));
+
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleClick = async () => {
+    setIsLoading(true);
+    await props.getRecipe();
+    setIsLoading(false);
+  }
 
   return (
     <section>
@@ -17,7 +27,9 @@ export default function IngredientsList(props) {
             <h3>Ready for a recipe?</h3>
             <p>Generate a recipe from your list of ingredients.</p>
           </div>
-          <button onClick={props.getRecipe}>Get a recipe</button>
+          <button onClick={handleClick}>
+            {isLoading ? "Loading..." : "Get a recipe"}
+          </button>
         </div>
       )}
     </section>
